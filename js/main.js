f17860776a8f95f9a0d193caec6e498a23352720
yerongshin <131427005+yerongshin@users.noreply.github.com>
2024-05-23 18:07:49 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const FULL_PARTICIPATION_FEE = 50000;
+const PARTIAL_PARTICIPATION_FEE_PER_ITEM = 9000;
+
+function checkAll(isChecked) {
+    document.querySelectorAll('.checkbox').forEach(checkbox => {
+        if (!checkbox.disabled) {
+            checkbox.checked = isChecked;
+        }
+    });
+}
+
+function calculateFee() {
+    console.log('Calculate function called');
+    let checkedCount = 0;
+    document.querySelectorAll('.checkbox').forEach(checkbox => {
+        if (checkbox.checked) {
+            checkedCount++;
+        }
+    });
+
+    let totalFee = checkedCount * PARTIAL_PARTICIPATION_FEE_PER_ITEM;
+    if (totalFee > FULL_PARTICIPATION_FEE) {
+        totalFee = FULL_PARTICIPATION_FEE;
+    }
+
+    document.getElementById('total').innerText = `총액은 ${totalFee.toLocaleString()}원 입니다.`;
+}
+
+// No need to wait for DOMContentLoaded as the script is at the end of the body
